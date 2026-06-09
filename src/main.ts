7c1129e5ea512827c7c0e82e22b9a01e7151f14b
OdenigboGodfrey <Odenigbogodfrey@yahoo.com>
2026-06-09 06:32:31 +0100

    deployment updates

--- src/main.ts
+++ src/main.ts
@@ -39,6 +39,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
