49d3bfc8697b9a0799eeebdbf0aa73e10a72b534
OdenigboGodfrey <Odenigbogodfrey@yahoo.com>
2026-06-29 13:59:40 +0100

    telegram bot WIP

--- src/modules/channels/channels.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TelegramService } from './services/telegram.service';
-import { TelegramController } from './controllers/telegram.controller';
-
-@Module({
-  controllers: [TelegramController],
-  providers: [TelegramService],
-})
-export class ChannelsModule {}
--- src/modules/chat/controllers/telegram.controller.ts
+++ src/modules/chat/controllers/telegram.controller.ts
@@ -1,14 +1,30 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Post,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { TelegramService } from '../services/telegram.service';
 import { ApiTags } from '@nestjs/swagger';
 
 @Controller('telegram-channel')
 @ApiTags('telegram-channel')
 export class TelegramController {
-  constructor(private telegram: TelegramService) {console.log('>>> TELEGRAM CONTROLLER INSTANTIATED');}
+  constructor(private telegram: TelegramService) {
+    console.log('>>> TELEGRAM CONTROLLER INSTANTIATED');
+  }
 
   @Post('webhook')
-  async webhook(@Body() body: any) {
+  async webhook(
+    @Body() body: any,
+    @Headers('x-telegram-bot-api-secret-token') secretToken: string,
+  ) {
+    if (secretToken !== process.env.TELEGRAM_SECRET_TOKEN) {
+      throw new UnauthorizedException('Invalid Telegram secret token');
+    }
+
     await this.telegram.handleUpdate(body);
 
     return { ok: true };
